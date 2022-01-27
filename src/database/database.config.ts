37fa4a2986440cbc8bdd8dc09add40735c5bdd98
ZajoncM <milosz.zajonc@gmail.com>
2022-01-27 14:08:10 +0100

    change database

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const DatabaseConfig = registerAs('database', () => ({
-  host: process.env.DATABASE_URL,
+  host: process.env.DATABASE_HOST,
 
   database: process.env.DATABASE_NAME,
 
